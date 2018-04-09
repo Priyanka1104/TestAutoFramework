3aa00ec955f419aef66d60551d44209ad1b779b7
Priyanka1104 <37972404+Priyanka1104@users.noreply.github.com>
2018-04-09 15:05:55 +0530

    Update EbayProductPurchaseSteps.java
    Comment for Step page added

--- src/test/java/com/ebay/steps/EbayProductPurchaseSteps.java
+++ src/test/java/com/ebay/steps/EbayProductPurchaseSteps.java
@@ -11,7 +11,7 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 import com.ebay.test.EbayProductPurchaseTest;
 import com.ebay.test.EbayProductPurchaseTest;
-//import com.ebay.test.tssBodyConfigurationPage;
+
 import com.ebay.utility.AppiumDriverLibraryImpl;
 import com.relevantcodes.extentreports.LogStatus;
 
@@ -20,16 +20,25 @@ import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 
-/**
- * Created by Priyanka .
- */
+/********************************************************************
+ * Class Name: EbayProductPurchaseSteps
+ * Generated : 07 April 2018
+ * Description: This class holds the various functions to map scenario steps to their step definition Java class files and records status as Pass or Fail. .
+ * Author : Priyanka
+ *******************************************************************/
 public class EbayProductPurchaseSteps extends AppiumDriverLibraryImpl{
 
-	
-
-	@Given("^User is on eBay landing page$")
+ /********************************************************************
+ * Function Name: verifyEbayLandingPage
+ * Generated : 07 April 2018
+ * Description: This method calls verifyEbayLandingPage method to verify user is on eBay Home page.
+ * Return type: void
+ * Author : Priyanka
+ *******************************************************************/	
+  
+    @Given("^User is on eBay landing page$")
     public void verifyEbayLandingPage() throws Throwable {
-     boolean landingStatus =  new EbayProductPurchaseTest(driver).verifyEbayLandingPage();
+    boolean landingStatus =  new EbayProductPurchaseTest(driver).verifyEbayLandingPage();
      if(landingStatus)
      {
     	 
@@ -41,8 +50,13 @@ public class EbayProductPurchaseSteps extends AppiumDriverLibraryImpl{
      }
     
     }  
-	
-	
+/********************************************************************
+ * Function Name: selectSignInBtn
+ * Generated : 07 April 2018
+ * Description: This method calls selectSignInBtn method to click on Sign in button.
+ * Return type : void
+ * Author : Priyanka
+ *******************************************************************/
 	@When("^User tap on Sign In button$")
     public void selectSignInBtn() throws Throwable {
 		boolean signInStatus = new EbayProductPurchaseTest(driver).selectSignInBtn();
@@ -62,6 +76,13 @@ public class EbayProductPurchaseSteps extends AppiumDriverLibraryImpl{
         
     }
 	
+ /********************************************************************
+ * Function Name: enterUserPassword
+ * Generated : 07 April 2018
+ * Description: This method calls enterUserPassword method to enter user name and password.
+ * Return type: void
+ * Author : Priyanka
+ *******************************************************************/	
 	@And("^User enters user name as \"([^\"]*)\" and password as \"([^\"]*)\"$")
     public void enterUserPassword(String userName, String password) throws Throwable {
       
@@ -79,7 +100,13 @@ public class EbayProductPurchaseSteps extends AppiumDriverLibraryImpl{
       	 logger.log(LogStatus.FAIL, "Not able to enter usr name or password");
        }
     }
-	
+ /********************************************************************
+ * Function Name: tapSignIn
+ * Generated : 07 April 2018
+ * Description: This method calls tapSubmit method to select Sign In button.
+ * Return type : void
+ * Author : Priyanka
+ *******************************************************************/	
 	@And("^user selects Sign In button to navigate on home page$")
     public void tapSignIn() throws Throwable {
         
@@ -97,7 +124,13 @@ public class EbayProductPurchaseSteps extends AppiumDriverLibraryImpl{
         }
         
     }
- 
+  /********************************************************************
+ * Function Name: enterProductSearch
+ * Generated : 07 April 2018
+ * Description: This method calls enterProductSearch method to enter product to be searched.
+ * Return type: void
+ * Author : Priyanka
+ *******************************************************************/
 	@And("^user search product to purchase from search text box as \"([^\"]*)\"$")
     public void enterProductSearch(String product) throws Throwable {
         
@@ -119,7 +152,13 @@ public class EbayProductPurchaseSteps extends AppiumDriverLibraryImpl{
         
         
     }
-	
+ /********************************************************************
+ * Function Name: selectProductSearch
+ * Generated : 07 April 2018
+ * Description: This method calls selectProductSearch method to select searched product.
+ * Return type : void
+ * Author : Priyanka
+ *******************************************************************/	
 	@And("^user select searched product$")
     public void selectProductSearch() throws Throwable {
        
@@ -138,7 +177,13 @@ public class EbayProductPurchaseSteps extends AppiumDriverLibraryImpl{
        }
        
     }
-	
+ /********************************************************************
+ * Function Name: selectBuyItBtn
+ * Generated : 07 April 2018
+ * Description: This method calls selectBuyItBtn method to tap on Buy it now button.
+ * Return type : void
+ * Author : Priyanka
+ *******************************************************************/	
 	@And("^user tap on Buy it now button$")
     public void selectBuyItBtn() throws Throwable {
         
@@ -156,7 +201,13 @@ public class EbayProductPurchaseSteps extends AppiumDriverLibraryImpl{
        	 logger.log(LogStatus.FAIL, "Not able to click on Buy it now button");
         }
     }
-	
+ /********************************************************************
+ * Function Name: selectReviewBtn
+ * Generated : 07 April 2018
+ * Description:  This method calls selectReviewBtn method to tap on Review option.
+ * Return type : void
+ * Author : Priyanka
+ *******************************************************************/	
 	@And("^user tap on Review option$")
     public void selectReviewBtn() throws Throwable {
         
@@ -174,7 +225,13 @@ public class EbayProductPurchaseSteps extends AppiumDriverLibraryImpl{
        	 logger.log(LogStatus.FAIL, "Not able to click on Select review button");
         }
     }
-	
+ /********************************************************************
+ * Function Name: selectProceedToPay
+ * Generated : 07 April 2018
+ * Description:  This method calls selectProceedToPay method to tap on proceed to pay option.
+ * Return type : void
+ * Author : Priyanka
+ *******************************************************************/	
 	@And("^user tap on proceed to pay option$")
     public void selectProceedToPay() throws Throwable {
         
@@ -192,6 +249,14 @@ public class EbayProductPurchaseSteps extends AppiumDriverLibraryImpl{
        	 logger.log(LogStatus.FAIL, "Not able to click on selected proceed to pay button");
         }
     }
+	
+ /********************************************************************
+ * Function Name: verifyPaymentPage
+ * Generated : 07 April 2018
+ * Description:  This method calls verifyPaymentPage method to verify thet user is on payment option page.
+ * Return type: void
+ * Author : Priyanka
+ *******************************************************************/
 	@Then("^user should get payment option page$")
     public void verifyPaymentPage() throws Throwable {
        
