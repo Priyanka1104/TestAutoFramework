690282986c9862c7d9240c259ee313f99561b74b
Priyanka1104 <37972404+Priyanka1104@users.noreply.github.com>
2018-04-09 15:30:53 +0530

    Update RunEbayTest.java
    Comment added for RunEbayTest Class

--- src/test/java/com/ebay/testRunner/RunEbayTest.java
+++ src/test/java/com/ebay/testRunner/RunEbayTest.java
@@ -3,9 +3,12 @@ package com.ebay.testRunner;
 import cucumber.api.CucumberOptions;
 import cucumber.api.testng.AbstractTestNGCucumberTests;
 
-/**
- * Created by Priyanka
- */
+/********************************************************************
+ * Class Name: RunEbayTest
+ * Generated : 07 April 2018
+ * Description: This class that configures Cucumber options and intigrates TestNG with Cucumber.
+ * Author : Priyanka
+ *******************************************************************/
 
 @CucumberOptions(features = "src/test/java/features/", plugin = { "listener.ExtentReporterNG" }, format = { "pretty",
 		"html:target/cucumber", "json:target/cucumber-report.json" }, tags = { "@mobile" }, glue = "steps"
